30e88480f32cf1536351faf34acfc79b4214c421
Aleksandra Niechaj <aleksandra.niechaj2@gmail.com>
2025-08-23 17:10:06 +0200

    Dodane CRUD dla przepisów i kategorie

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -8,8 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(RecipeRepository $recipeRepo, CategoryRepository $categoryRepo): Response
     {
-        return $this->render('home/index.html.twig');
+        // pobieramy wszystkie kategorie
+        $categories = $categoryRepo->findAll();
+
+        // pobieramy 3 najnowsze przepisy
+        $recipes = $recipeRepo->findBy([], ['createdAt' => 'DESC'], 3);
+
+        return $this->render('home/index.html.twig', [
+            'categories' => $categories,
+            'recipes' => $recipes,
+        ]);
     }
 }
--- app/src/Controller/RecipeController.php
+++ app/src/Controller/RecipeController.php
@@ -3,15 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Recipe;
+use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/recipe')]
 class RecipeController extends AbstractController
 {
-    // lista (przyda się do testów, później dodamy paginację 10/s)
+    // lista (później dodamy paginację)
     #[Route('/', name: 'app_recipe_index', methods: ['GET'])]
     public function index(RecipeRepository $repo): Response
     {
@@ -22,7 +25,63 @@ class RecipeController extends AbstractController
         ]);
     }
 
-    // szczegóły po ID (ParamConverter wstrzyknie encję)
+    // nowy przepis
+    #[Route('/new', name: 'app_recipe_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $now = new \DateTimeImmutable();
+            $recipe->setCreatedAt($now);
+            $recipe->setUpdatedAt($now);
+
+            $em->persist($recipe);
+            $em->flush();
+
+            return $this->redirectToRoute('app_recipe_index');
+        }
+
+        return $this->render('recipe/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    // edycja
+    #[Route('/{id}/edit', name: 'app_recipe_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Recipe $recipe, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recipe->setUpdatedAt(new \DateTimeImmutable());
+            $em->flush();
+
+            return $this->redirectToRoute('app_recipe_index');
+        }
+
+        return $this->render('recipe/edit.html.twig', [
+            'form' => $form->createView(),
+            'recipe' => $recipe,
+        ]);
+    }
+
+    // usuwanie
+    #[Route('/{id}/delete', name: 'app_recipe_delete', methods: ['POST'])]
+    public function delete(Request $request, Recipe $recipe, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$recipe->getId(), $request->request->get('_token'))) {
+            $em->remove($recipe);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('app_recipe_index');
+    }
+
+    // szczegóły
     #[Route('/{id}', name: 'recipe_show', methods: ['GET'])]
     public function show(Recipe $recipe): Response
     {

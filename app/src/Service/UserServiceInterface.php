d32b56b132a3da20fdef5d6fd31decc59a63ff49
Aleksandra Niechaj <aleksandra.niechaj2@gmail.com>
2025-09-07 00:20:49 +0200

    UDAŁO SIE PIERWSZĄ RZECZ USUNĄC CAŁKOWICIE Z OBU FIXXERA I SNIFFERA USER SERVICE INTERFACE

--- app/src/Service/UserServiceInterface.php
+++ app/src/Service/UserServiceInterface.php
@@ -3,13 +3,7 @@
 declare(strict_types=1);
 
 /**
- * This file is part of the Cook Book project.
- *
- * PHP version 8.3
- *
- * @author    Aleksandra Niechaj <aleksandra.niechaj@example.com>
- * @copyright 2025 Aleksandra Niechaj
- * @license   For educational purposes (course project).
+ * Interfejs serwisu użytkowników.
  */
 
 namespace App\Service;
